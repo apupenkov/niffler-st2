f71ccd67cadb69b28efd191c3cc0024a993987dd
Fireoldbeard <pupenkov.tech@gmail.com>
2023-06-12 21:35:18 +0300

    add LoginPage and FriendsPage

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/FriendsPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/FriendsPage.java
@@ -1,13 +1,30 @@
 package guru.qa.niffler.page;
 
+import com.codeborne.selenide.ElementsCollection;
 import guru.qa.niffler.page.component.Header;
+import io.qameta.allure.Step;
 
-public class FriendsPage {
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class FriendsPage extends BasePage<FriendsPage>{
 
   private final Header header = new Header();
+  private final ElementsCollection firendsTable = $$(".table tbody tr");
 
   public Header getHeader() {
     return header;
+  }
+
+  @Override
+  public FriendsPage checkThatPageLoaded() {
+    return null;
+  }
+
+  @Step("Verify Friends table not empty")
+  public FriendsPage verifyFriendsTableNotEmpty() {
+    firendsTable.shouldHave(sizeGreaterThan(0));
+    return this;
   }
 
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/LoginPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/LoginPage.java
@@ -1,25 +1,63 @@
 package guru.qa.niffler.page;
 
-import com.codeborne.selenide.Conditional;
 import com.codeborne.selenide.SelenideElement;
 import guru.qa.niffler.model.UserJson;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage extends BasePage<LoginPage>{
 
-    private final SelenideElement username = $("input[name='username']");
-    private final SelenideElement goToLoginFormBtn = $("a[href*='redirect']");
-    private final SelenideElement password = $("input[name='password']");
-    private final SelenideElement loginBtn = $("button[type='submit']");
-    private final SelenideElement header = $(".form__header");
+    private final SelenideElement usernameField = $("input[name='username']"),
+        goToLoginFormBtn = $("a[href*='redirect']"),
+        passwordField = $("input[name='password']"),
+        loginBtn = $("button[type='submit']"),
+        header = $(".form__header");
 
     public void login(UserJson user) {
-        goToLoginFormBtn.click();
-        username.setValue(user.getUsername());
-        password.setValue(user.getPassword());
+        clickLoginBtn()
+            .enterUsername(user.getUsername())
+            .enterPassword(user.getPassword())
+            .clickSignInBtn();
+    }
+
+    @Step("Нажать на кнопку входа")
+    public LoginPage clickLoginBtn() {
         loginBtn.click();
+        return this;
+    }
+
+    @Step("Нажать на кнопку регистрации")
+    public RegistrationPage clickRegisterBtn() {
+        $(byText("Register")).click();
+        return new RegistrationPage();
+    }
+
+    @Step("Ввести имя пользователя")
+    public LoginPage enterUsername(String username) {
+        usernameField.setValue(username);
+        return this;
+    }
+
+    @Step("Ввести пароль")
+    public LoginPage enterPassword(String password) {
+        passwordField.setValue(password);
+        return this;
+    }
+
+    @Step("Нажать на кнопку входа")
+    public LoginPage clickSignInBtn() {
+        $(byText("Sign In")).click();
+        return this;
+    }
+
+    @Step("Нажать на кнопку выхода")
+    public LoginPage clickSignUp() {
+        $(byText("Sign up!")).click();
+        return this;
     }
 
     @Override
